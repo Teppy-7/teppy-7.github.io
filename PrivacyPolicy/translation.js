660a2cd78c97d5f9077d83c7694bfcdc9f3e6ab7
Teppy-7 <91276744+Teppy-7@users.noreply.github.com>
2024-04-10 22:39:33 +0900

    Add files via upload

--- PrivacyPolicy/translation.js
+++ PrivacyPolicy/translation.js
@@ -1,17 +1,31 @@
-//言語変更 英語・日本語
-document.getElementById('language-select').addEventListener('change', function() {
+document.addEventListener('DOMContentLoaded', function() {
+  var languageSelect = document.getElementById('language-select');
+  languageSelect.value = 'en'; // デフォルトの値を設定
+ 
+  var language = languageSelect.value;
+  var elements = document.querySelectorAll('[data-ja], [data-en]');
+ 
+  elements.forEach(function(element) {
+     if (language === 'ja' && element.hasAttribute('data-ja')) {
+       element.textContent = element.getAttribute('data-ja');
+     } else if (language === 'en' && element.hasAttribute('data-en')) {
+       element.textContent = element.getAttribute('data-en');
+     }
+     // 言語が切り替わったときに、data-jaやdata-en属性がない要素に対しては何もしない
+  });
+ });
+ 
+ // 言語が切り替わったときにも同様の処理を行う
+ document.getElementById('language-select').addEventListener('change', function() {
   var language = this.value;
-  var content = document.querySelectorAll('.content');
-  var headers = document.querySelectorAll('h1, h2, p');
-
-  // ここで、各要素のテキストを言語に応じて変更します。
-  // 例えば、各ヘッダーと段落のテキストを日本語と英語のバージョンに切り替えることができます。
-  // この例では、単純化のために、すべてのテキストを単一の言語に設定しています。
-  content.forEach(function(element) {
-      element.lang = language;
+  var elements = document.querySelectorAll('[data-ja], [data-en]');
+ 
+  elements.forEach(function(element) {
+     if (language === 'ja' && element.hasAttribute('data-ja')) {
+       element.textContent = element.getAttribute('data-ja');
+     } else if (language === 'en' && element.hasAttribute('data-en')) {
+       element.textContent = element.getAttribute('data-en');
+     }
+     // 言語が切り替わったときに、data-jaやdata-en属性がない要素に対しては何もしない
   });
-
-  headers.forEach(function(element) {
-      element.lang = language;
-  });
-});
+ });
